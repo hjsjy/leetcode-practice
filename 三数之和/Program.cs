f9b3e1bd89177c7b6d2ed207ca66c53961195290
narojay <hj200812@126.com>
2020-06-12 20:02:17 +0800

    add array question :  3Sum

---
+++ 三数之和/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 三数之和
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -1, 0, 1, 2, -1, -4 };
+
+            ThreeSum(nums);
+        }
+
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums);
+            var result = new List<IList<int>>();
+            int len = nums.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                int left = i + 1;
+                int right = len - 1;
+                while (left < right)
+                {
+                    int sum = nums[i] + nums[left] + nums[right];
+                    if (sum == 0)
+                    {
+                        result.Add(new List<int> { nums[i], nums[left], nums[right] });
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    }
+                    else if (sum < 0) left++;
+                    else if (sum > 0) right--;
+                }
+            }
+            return result;
+        }
+    }
+}
