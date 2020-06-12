7c8b958a7647602780a63795b69890cd61d395cb
narojay <hj200812@126.com>
2020-06-12 17:38:41 +0800

    add leetocde question

---
+++ 两数之和/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 两数之和
+{
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = {2, 7, 11, 15};
+
+            var twoSum = TwoSum(nums, 9);
+        }
+
+        /// <summary>
+        /// 通过目标值与数组的差值，保存到dictionary比较，差值在数组中存在并且不重复
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public  static int[] TwoSum(int[] nums, int target)
+        {
+            var dictionary = new Dictionary<int,int>();
+            for (int i = 0; i < nums.Length ; i++)
+            {
+                var num = target - nums[i];
+                if (dictionary.ContainsKey(num) && dictionary[num] != i)
+                {
+                    return new int[]{i,dictionary[num]};
+                }
+                if (!dictionary.ContainsKey(nums[i]))
+                {
+                    dictionary.Add(nums[i], i);
+                }
+            }
+            return new[] {0, 0};
+        }
+    }
+}
---
+++ 每日温度/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+
+namespace 每日温度
+{
+    /// <summary>
+    ///请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+
+    ///例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是[1, 1, 4, 2, 1, 1, 0, 0]。
+
+    ///提示：气温 列表长度的范围是[1, 30000]。每个气温的值的均为华氏度，都是在[30, 100] 范围内的整数。
+
+    ///通过次数78,825提交次数123,995
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 5, 4, 2, 3, 69, 72, 76, 73 };
+            DailyTemperatures(num);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="T"></param>
+        /// <returns></returns>
+        public static int[] DailyTemperatures(int[] T)
+        {
+            int len = T.Length;
+            var stack = new Stack<int>();
+            int[] result = new int[len];
+            for (int i = len - 1; i >= 0; i--)
+            {
+                while (stack.Count != 0 && T[stack.Peek()] <= T[i])
+                    stack.Pop();
+                if (stack.Count != 0)
+                    result[i] = stack.Peek() - i;
+                stack.Push(i);
+            }
+            return result;
+
+
+
+
+            // int len = T.Length;
+            // Stack<int> stack = new Stack<int>();
+            // int[] result = new int[len];
+            // for (int i = len - 1; i >= 0; i--)
+            // {
+            //     while (stack.Count != 0 && T[stack.Peek()] <= T[i])
+            //         stack.Pop();
+            //     if (stack.Count != 0)
+            //         result[i] = stack.Peek() - i;
+            //     stack.Push(i); }
+            // return result;
+            // Stack<int> stack = new Stack<int>();
+            // int[] ret = new int[T.Length];
+            // for (int i = T.Length - 1; i> 0;   i--)
+            // {
+            //     while (stack.Count > 0 && T[i] < T[stack.Peek()])
+            //     {
+            //         var pop = stack.Pop();
+            //         ret[i] =pop -i;
+            //     }
+            //     stack.Push(i);
+            // }
+            //
+            // return ret;
+
+            // Stack<int> stack = new Stack<int>();
+            // int[] ret = new int[T.Length];
+            // ret[T.Length - 1] = 0;
+            // for (int i = 0; i < T.Length; i++)
+            // {
+            //     while (stack.Count > 0 && T[i] > T[stack.Peek()])
+            //     {
+            //         int tmp = stack.Pop();
+            //         ret[tmp] = i - tmp;
+            //     }
+            //     stack.Push(i);
+            // }
+            // return ret;
+        }
+    }
+
+}
