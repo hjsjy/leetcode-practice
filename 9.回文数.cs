32ecc41f0b72b2e9d748127e9b4e28a383ff3143
narojay <hj200812@126.com>
2020-07-19 17:43:56 +0800

    回文数

---
+++ 9.回文数.cs
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=csharp
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+public class Solution {
+    public bool IsPalindrome(int x) {
+      if(x < 0 || (x%10 ==0 && x != 0 )) return false;
+     int revertNumber =  0;
+     while (x >  revertNumber ){
+         revertNumber = revertNumber *10 + x %10;
+         x /= 10;
+     }
+        return  x == revertNumber || x == revertNumber/10;
+    }
+}
+// @lc code=end
+
