2892f63abd1f79ed2716c71782218f031aef1e94
narojay <hj200812@126.com>
2020-08-09 00:15:26 +0800

    爬楼梯 动态规划

---
+++ 70.爬楼梯.cs
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=csharp
+ *
+ * [70] 爬楼梯 （动态规划）
+ */
+
+
+// @lc code=start
+public class Solution {
+    public int ClimbStairs(int n) {
+        int  p, q = 0;
+        int result  = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            p = q ;
+            q =result ;
+            result = p +q;
+        }
+        return result;
+    }
+}
+// @lc code=end
+
